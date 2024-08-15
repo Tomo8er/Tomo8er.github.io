228626c7826fa84127874791c6bb663c46a04a43
Tomo8er <tomicamonmo@outlook.jp>
2024-08-15 13:22:12 +0900

    programs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
   const searchButton = document.getElementById('search-button');
   const searchInput = document.getElementById('search-input');
   const searchResults = document.getElementById('search-results');
@@ -26,38 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // 現在の年を設定
-  document.getElementById('current-year').textContent = new Date().getFullYear();
-});
+});*/
 
-        // Cookieを設定する関数
-        function setCookie(name, value, days) {
-          var expires = "";
-          if (days) {
-              var date = new Date();
-              date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-              expires = "; expires=" + date.toUTCString();
-          }
-          document.cookie = name + "=" + (value || "") + expires + "; path=/";
-      }
-
-      // Cookieを取得する関数
-      function getCookie(name) {
-          var nameEQ = name + "=";
-          var ca = document.cookie.split(';');
-          for (var i = 0; i < ca.length; i++) {
-              var c = ca[i];
-              while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-              if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-          }
-          return null;
-      }
-
-      // 初めての訪問者かどうかを判定してダイアログを表示する
-      function checkFirstVisit() {
-          var visited = getCookie("visited");
-          if (!visited) {
-              alert("To our first time visitors, don't come here.")
-              setCookie("visited", "true", 365);  // 1年間有効なCookieを設定
-          }
-      }
+document.getElementById('current-year').textContent = new Date().getFullYear();
